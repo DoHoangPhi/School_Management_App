49cef399efd8d888eb2f3af28aba48ea8ac31ddb
Do Hoang Phi <ih6bmt@gmail.com>
2026-03-20 06:50:00 +0700

    feat: add dashboard list pages for classes, events, exams, lessons, parents, results, students, subjects, and teachers
    - Implemented ClassListPage with pagination and search functionality.
    - Created EventListPage to display events with actions for admin users.
    - Developed ExamListPage to manage exam details and actions.
    - Added LessonListPage for lesson management with search and pagination.
    - Introduced ParentListPage to list parents with their details and actions.
    - Created ResultListPage to display student results with actions for admin users.
    - Implemented StudentListPage to manage student information and actions.
    - Developed SubjectListPage to list subjects and associated actions.
    - Created TeacherListPage to manage teacher information with actions.
    - Added reusable components: Pagination, Table, TeacherSearch, and BigCalendar.

---
+++ app/(dashboard)/list/assignments/page.tsx
@@ -0,0 +1,102 @@
+import Pagination from '@/components/Pagination'
+import Table from '@/components/Table'
+import TeacherSearch from '@/components/TeacherSearch'
+import { assignmentsData, classesData, examsData, lessonsData, parentsData, role } from '@/lib/data'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type Assignment = {
+    id: number,
+    subject: string,
+    class: number,
+    teacher: number,
+    dueDate: string,
+}
+
+const colums = [
+    {
+        header: 'Subject', accessor: 'subject'
+    },
+    {
+        header: 'Class', accessor: 'class', className: "hidden md:table-cell"
+    },
+    {
+        header: 'Teacher', accessor: 'teacher', className: "hidden md:table-cell"
+    },
+    {
+        header: 'DueDate', accessor: 'dueDate', className: "hidden md:table-cell"
+    },
+    {
+        header: 'Actions', accessor: 'actions'
+    },
+]
+
+const AssignmentListPage = () => {
+
+    const renderRow = (item: Assignment) => (
+        <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPureLight'>
+            <td className='flex items-center gap-4 p-4'>
+                <div className='flex flex-col'>
+                    <h3 className='font-semibold'>{item.subject}</h3>
+                </div>
+            </td>
+
+            <td className='hidden md:table-cell'>
+                {item.class}
+            </td>
+            <td className='hidden md:table-cell'>
+                {item.teacher}
+            </td>
+            <td className='hidden md:table-cell'>
+                {item.dueDate}
+            </td>
+            <td>
+                <div className='flex items-center gap-2'>
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className='w-7 h-7 flex items-center bg-lamaSky justify-center rounded-full'>
+                            <Image src={`/edit.png`} alt='' width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role === 'admin' && (
+                        <button className='w-7 h-7 flex items-center bg-lamaPure justify-center rounded-full'>
+                            <Image src={`/delete.png`} alt='' width={16} height={16} />
+                        </button>
+                    )}
+                </div>
+            </td>
+        </tr>
+    )
+
+    return (
+        <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+            {/* TOP */}
+            <div className='flex items-center justify-between'>
+                <h1 className='hidden md:block text-lg font-semibold'>All Assignments</h1>
+                <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
+                    <TeacherSearch />
+                    <div className='flex items-center gap-4 self-end'>
+                        <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                            <Image src='/filter.png' alt='filter' width={14} height={14} />
+                        </button>
+                        <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                            <Image src='/sort.png' alt='filter' width={14} height={14} />
+                        </button>
+                        {role === 'admin' && (
+                            <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+                                <Image src='/plus.png' alt='filter' width={14} height={14} />
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </div>
+            {/* LIST */}
+            <Table columns={colums} renderRow={renderRow} data={assignmentsData} />
+            {/* PAGINATION */}
+            <Pagination />
+
+        </div>
+    )
+}
+
+export default AssignmentListPage
